f984fb5d6f59303d03a8dfce920d231d494d5beb
Hui Peng <peng124@purdue.edu>
2021-03-07 05:57:35 -0500

    Remove some garbage code and improve readability

--- lib/Graphs/VFG.cpp
+++ lib/Graphs/VFG.cpp
@@ -412,7 +412,7 @@ void VFG::addVFGNodes()
 
         for(const auto *param : it.second)
         {
-             if (isInterestedPAGNode(param) == false || hasBlackHoleConstObjAddrAsDef(param))
+             if (!isInterestedPAGNode(param) || hasBlackHoleConstObjAddrAsDef(param))
                 continue;
 
             CallPESet callPEs;
