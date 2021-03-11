bcdde53635913d0086dff07e9945f502ee1fc2c1
Hui Peng <peng124@purdue.edu>
2021-03-11 00:05:28 -0500

    Continue refactoring

---
+++ include/SVF-FE/SVFProject.h
@@ -0,0 +1,20 @@
+#ifndef __SVFPROJECT_H_
+#define __SVFPROJECT_H_
+
+
+#include "SVF-FE/SymbolTableInfo.h"
+#include "Util/SVFModule.h"
+
+namespace SVF
+{
+
+class SVFProject {
+private:
+    SVFModule *svfModule;
+    SymbolTableInfo *symTblInfo;
+};
+
+} // end of namespace SVF
+
+
+#endif // __SVFPROJECT_H_
