3f9103e60c635eeb42c7268b2ddca3d889b5b6fa
Hui Peng <peng124@purdue.edu>
2021-04-14 21:25:08 -0400

    Update unit test for symbol table

--- unittests/SymbolTableInfo/SymbolTableInfo-unittests.cpp
+++ unittests/SymbolTableInfo/SymbolTableInfo-unittests.cpp
@@ -41,7 +41,7 @@ class SymbolTableInfoTest : public ::testing::Test {
             }
         }
 
-        void common_symbol_table_tests(string &bc_file) {
+        void cmp_symtableid_and_pagid(string &bc_file) {
             init_if_needed(bc_file);
 
             SymbolTableInfo *symInfo = p_proj->getSymbolTableInfo();
@@ -66,40 +66,89 @@ class SymbolTableInfoTest : public ::testing::Test {
             // here we test that the same function always gets the
             // same id
             //
-            map<string, SymID> fval_id_map;
-            map<string, SymID> fobj_id_map;
             SVFModule *mod = p_proj->getSVFModule();
-            for (auto it = mod->llvmFunBegin();
-                 it != mod->llvmFunEnd(); it++) {
-                Function *fun = *it;
-                string fname = fun->getName().str();
-                llvm::outs() << "function name:`" << fname << "`\n";
-                llvm::outs() << "demangled function name:`"
-                             << llvm::demangle(fname) << "`\n";
-                ASSERT_TRUE(symInfo->hasValSym(fun));
-                llvm::outs() << "Val Sym ID:" << symInfo->getValSym(fun) << "\n";
-                llvm::outs() << "Obj Sym ID:" << symInfo->getObjSym(fun) << "\n";
-
-                fval_id_map[fname] = symInfo->getValSym(fun);
-                fobj_id_map[fname] = symInfo->getObjSym(fun);
-            }
-
             // here we create another project from the same file
             SVFProject proj2(bc_file);
             SVFModule *mod2 = proj2.getSVFModule();
             ASSERT_NE(mod, mod2);
-
             SymbolTableInfo *symInfo2 = proj2.getSymbolTableInfo();
             ASSERT_NE(symInfo, symInfo2);
-            for (auto it = mod2->llvmFunBegin();
-                 it != mod2->llvmFunEnd(); it++) {
-                Function *fun = *it;
-                string fname = fun->getName().str();
-                ASSERT_TRUE(symInfo2->hasValSym(fun));
 
-                ASSERT_EQ(symInfo2->getValSym(fun), fval_id_map[fname]);
-                ASSERT_EQ(symInfo2->getObjSym(fun), fobj_id_map[fname]);
+            // check globals
+            for (auto it1 = mod->global_begin(), it2 = mod2->global_begin();
+                 it1 != mod->global_end() && it2 != mod2->global_end();
+                 it1 ++, it2 ++) {
+                ASSERT_NE(*it1, *it2);
+                ASSERT_TRUE(symInfo->hasValSym(*it1));
+                ASSERT_TRUE(symInfo2->hasValSym(*it2));
+                ASSERT_EQ(symInfo->getValSym(*it1),
+                          symInfo2->getValSym(*it2));
+
             }
+            // check aliases
+            for (auto it1 = mod->alias_begin(), it2 = mod2->alias_begin();
+                 it1 != mod->alias_end() && it2 != mod2->alias_end();
+                 it1 ++, it2++) {
+                ASSERT_NE(*it1, *it2);
+                ASSERT_TRUE(symInfo->hasValSym(*it1));
+                ASSERT_TRUE(symInfo2->hasValSym(*it2));
+                ASSERT_EQ(symInfo->getValSym(*it1),
+                          symInfo2->getValSym(*it2));
+            }
+
+
+            auto it1 = mod->llvmFunBegin(), it2 = mod2->llvmFunBegin();
+            for (; it1 != mod->llvmFunEnd() && it2 != mod2->llvmFunEnd();
+                  it1 ++, it2 ++) {
+                Function *fun1 = *it1;
+                Function *fun2 = *it2;
+
+                ASSERT_NE(fun1, fun2);
+                ASSERT_STREQ(fun1->getName().str().c_str(),
+                             fun2->getName().str().c_str());
+
+                ASSERT_TRUE(symInfo->hasValSym(fun1));
+                ASSERT_TRUE(symInfo2->hasValSym(fun2));
+
+                ASSERT_EQ(symInfo->getValSym(fun1),
+                          symInfo2->getValSym(fun2));
+
+                auto bb1 = fun1->begin(), bb2 = fun2->begin();
+                for (; bb1 != fun1->end() && bb2 != fun2->end();
+                     bb1 ++, bb2 ++) {
+
+                    BasicBlock *pbb1 = &(*bb1), *pbb2 = &(*bb2);
+
+                    ASSERT_NE(pbb1, pbb2);
+
+                    ASSERT_TRUE(symInfo->hasValSym(pbb1));
+                    ASSERT_TRUE(symInfo2->hasValSym(pbb2));
+
+                    ASSERT_EQ(symInfo->getValSym(pbb1),
+                              symInfo2->getValSym(pbb2));
+
+                    auto inst1 = pbb1->begin(), inst2 = pbb2->begin();
+                    for (; inst1 != pbb1->end() && inst2 != pbb2->end();
+                         inst1 ++, inst2 ++) {
+                        Instruction *pinst1 = &(*inst1),*pinst2 = &(*inst2);
+                        ASSERT_NE(pinst1, pinst2);
+                        ASSERT_TRUE(symInfo->hasValSym(pinst1));
+                        ASSERT_TRUE(symInfo2->hasValSym(pinst2));
+
+                        ASSERT_EQ(symInfo->getValSym(pinst1),
+                                  symInfo2->getValSym(pinst2));
+                    }
+
+                    ASSERT_EQ(inst1, pbb1->end());
+                    ASSERT_EQ(inst2, pbb2->end());
+                }
+
+                ASSERT_EQ(bb1, fun1->end());
+                ASSERT_EQ(bb2, fun2->end());
+            }
+
+            ASSERT_EQ(it1, mod->llvmFunEnd());
+            ASSERT_EQ(it2, mod2->llvmFunEnd());
 
             // TODO: more tests on other values,
             // e.g., global variables, internal values etc.
@@ -150,10 +199,19 @@ class SymbolTableInfoTest : public ::testing::Test {
             ASSERT_EQ(varargSyms_1.size(), varargSyms_2.size());
 
             // TODO: more tests
+
         }
 
-        void cmp_symtableid_and_pagid(string &bc_file) {
+        void common_symbol_table_tests(string &bc_file) {
             init_if_needed(bc_file);
+            ASSERT_NE(p_proj->getSVFModule(), nullptr);
+            ASSERT_NE(p_proj->getLLVMModSet(), nullptr);
+
+            SymbolTableInfo *symInfo = p_proj->getSymbolTableInfo();
+
+            ASSERT_NE(symInfo, nullptr);
+            ASSERT_NE(symInfo->getModule(), nullptr);
+            llvm::outs() << symInfo->getTotalSymNum() << "\n";
         }
 };
 
@@ -166,31 +224,24 @@ TEST_F(SymbolTableInfoTest, Construction_0) {
 
 TEST_F(SymbolTableInfoTest, Construction_1) {
     string ll_file = SVF_BUILD_DIR "tests/CHG/callsite_cpp.ll";
-    SVFProject proj(ll_file);
-    ASSERT_NE(proj.getSVFModule(), nullptr);
-    ASSERT_NE(proj.getLLVMModSet(), nullptr);
-
-    SymbolTableInfo *symInfo = proj.getSymbolTableInfo();
-
-    ASSERT_NE(symInfo, nullptr);
-    ASSERT_TRUE(symInfo->getModule());
-    llvm::outs() << symInfo->getTotalSymNum() << "\n";
+    common_symbol_table_tests(ll_file);
+    cmp_symtableid_and_pagid(ll_file);
 }
 
 TEST_F(SymbolTableInfoTest, Construction_2) {
     string ll_file = SVF_BUILD_DIR "tests/CPPUtils/Vtable/PureVirtual_cpp.ll";
-    SVFProject proj(ll_file);
-    ASSERT_NE(proj.getSVFModule(), nullptr);
-    ASSERT_NE(proj.getLLVMModSet(), nullptr);
-
-    SymbolTableInfo *symInfo = proj.getSymbolTableInfo();
-
-    ASSERT_NE(symInfo, nullptr);
-
-    ASSERT_TRUE(symInfo->getModule());
-
-    llvm::outs() << symInfo->getTotalSymNum() << "\n";
+    common_symbol_table_tests(ll_file);
+    cmp_symtableid_and_pagid(ll_file);
 }
+
+
+TEST_F(SymbolTableInfoTest, Construction_WebGL_IR) {
+    string ll_file = SVF_SRC_DIR
+        "tools/chrome-gl-analysis/chrome_webgl_ir/webgl_all_rendering_code.bc";
+    common_symbol_table_tests(ll_file);
+    cmp_symtableid_and_pagid(ll_file);
+}
+
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest();
