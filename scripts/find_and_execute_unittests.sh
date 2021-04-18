df03d94a571cdab9e086fa53b2a8b492f8cee854
Hui Peng <peng124@purdue.edu>
2021-04-18 07:28:12 -0400

    Sqashed Commits: 1. Update bitbucket-pipelines.yml    skip code format checking and add execution of unittests 2. Update find_and_execute_unittests.sh    Signal failed cases properly

--- scripts/find_and_execute_unittests.sh
+++ scripts/find_and_execute_unittests.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-for test in `find unittests -executable -type f`; do
+Failed_cases=()
+Failed=0
+for test in $(find unittests -executable -type f); do
     $test
+
+    if [ $? -ne 0 ]; then
+        Failed_cases+=("$test")
+        Failed=1
+    fi
 done
+
+if [ $Failed -ne 0 ]; then
+    echo "****** The following cases did not pass:"
+    for fc in  "${Failed_cases[@]}"; do
+        echo "- ${fc}"
+    done
+
+    exit 1
+fi
+
+exit 0
