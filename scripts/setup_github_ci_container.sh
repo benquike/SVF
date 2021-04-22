3af47ac6ae82394030e117b66a3476095b9d00f0
Hui Peng <peng124@purdue.edu>
2021-04-21 23:41:31 -0400

    Fix the script for github workflow
    install boost library in the virtual env

--- scripts/setup_github_ci_container.sh
+++ scripts/setup_github_ci_container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt install -y clang-10 clang-tools-10
-
+sudo apt install -y libboost-serialization-dev libboost-program-options-dev
+sudo apt install -y libboost-system-dev libboost-filesystem-dev
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 40  \
     --slave /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-10 \
     --slave /usr/bin/opt opt /usr/bin/opt-10 \
