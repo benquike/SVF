c98897350b7c5ec765db6eb15cf226fa55df15cc
Hui Peng <peng124@purdue.edu>
2021-04-29 02:39:48 -0400

    Add unittests for casting

---
+++ unittests/Cast/cast_unittests.cpp
@@ -0,0 +1,206 @@
+/******************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Author:
+ *     Hui Peng <peng124@purdue.edu>
+ * Date:
+ *     2021-04-26
+ *****************************************************************************/
+
+#include <memory>
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "Util/Casting.h"
+#include "Graphs/ICFG.h"
+#include "Graphs/ConsG.h"
+#include "Graphs/PAG.h"
+#include "Graphs/VFG.h"
+#include "Graphs/SVFG.h"
+#include "Graphs/SVFGOPT.h"
+#include "Graphs/PTACallGraph.h"
+#include "Graphs/OfflineConsG.h"
+#include "Graphs/ThreadCallGraph.h"
+#include "SVF-FE/CHG.h"
+#include "SVF-FE/DCHG.h"
+
+using namespace SVF;
+using namespace std;
+
+
+#define define_cast_test(FROM_TYPE, TO_TYPE)    \
+    {                                           \
+        auto *_p = new FROM_TYPE();             \
+        ASSERT_TRUE(llvm::isa<TO_TYPE>(_p));    \
+        auto *_q = llvm::dyn_cast<TO_TYPE>(_p); \
+        auto *_r = llvm::cast<TO_TYPE>(_p);     \
+        delete _p;                              \
+    }                                           \
+    {                                           \
+        auto _p = make_unique<FROM_TYPE>();     \
+        ASSERT_TRUE(llvm::isa<TO_TYPE>(_p));    \
+        auto _q = llvm::dyn_cast<TO_TYPE>(_p);  \
+        auto _r = llvm::cast<TO_TYPE>(_p);      \
+    }                                           \
+    {                                           \
+        auto _p = make_shared<FROM_TYPE>();     \
+        ASSERT_TRUE(llvm::isa<TO_TYPE>(_p));    \
+        auto _q = llvm::dyn_cast<TO_TYPE>(_p);  \
+        auto _r = llvm::cast<TO_TYPE>(_p);      \
+    }
+
+
+TEST(CastTestSuite, auto_raw_ptr) {
+    #include "casting_test_rawptr.h"
+}
+
+TEST(CastTestSuite, auto_uinque_ptr) {
+    // #include "casting_test_unique_ptr.h"
+}
+
+TEST(CastTestSuite, auto_shared_ptr) {
+    #include "casting_test_shared_ptr.h"
+}
+
+
+TEST(CastTestSuite, raw_ptr_basic_0) {
+    ICFGNode *globalNode = new GlobalBlockNode();
+    ICFGNode *funEntryNode = new FunEntryBlockNode();
+    ICFGNode *funExitNode = new FunExitBlockNode();
+
+    ASSERT_TRUE(llvm::isa<ICFGNode>(globalNode));
+    ASSERT_TRUE(llvm::isa<GlobalBlockNode>(globalNode));
+    ASSERT_FALSE(llvm::isa<InterBlockNode>(globalNode));
+    ASSERT_FALSE(llvm::isa<IntraBlockNode>(globalNode));
+    ASSERT_TRUE(llvm::isa<ICFGNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<InterBlockNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<FunEntryBlockNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<ICFGNode>(funExitNode));
+    ASSERT_TRUE(llvm::isa<InterBlockNode>(funExitNode));
+    ASSERT_TRUE(llvm::isa<FunExitBlockNode>(funExitNode));
+    ASSERT_FALSE(llvm::isa<FunEntryBlockNode>(funExitNode));
+    ASSERT_FALSE(llvm::isa<CallBlockNode>(funExitNode));
+    ASSERT_FALSE(llvm::isa<RetBlockNode>(funExitNode));
+
+    // the following code should work at runtime
+    GlobalBlockNode *g_c = llvm::dyn_cast<GlobalBlockNode>(globalNode);
+    ASSERT_NE(g_c, nullptr);
+    FunEntryBlockNode *fentry = llvm::dyn_cast<FunEntryBlockNode>(funEntryNode);
+    ASSERT_NE(fentry, nullptr);
+    FunExitBlockNode *fexit = llvm::dyn_cast<FunExitBlockNode>(funExitNode);
+    ASSERT_NE(fexit, nullptr);
+    FunEntryBlockNode *fg_c = llvm::dyn_cast<FunEntryBlockNode>(globalNode);
+    ASSERT_EQ(fg_c, nullptr);
+
+    delete funExitNode;
+    delete funEntryNode;
+    delete globalNode;
+}
+
+TEST(CastTestSuite, unique_ptr_basic_0) {
+    // define_cast_test(GlobalBlockNode, ICFGNode)
+    unique_ptr<ICFGNode> node = make_unique<GlobalBlockNode>();
+    ASSERT_TRUE(llvm::isa<ICFGNode>(node));
+    ASSERT_TRUE(llvm::isa<GlobalBlockNode>(node));
+    // unique_ptr<ICFGNode> icfgNode = llvm::dyn_cast<ICFGNode>(node);
+    ASSERT_NE(node.get(), nullptr);
+    auto addr0 = node.get();
+    unique_ptr<ICFGNode> icfgNode = llvm::unique_dyn_cast<ICFGNode>(node);
+    ASSERT_EQ(addr0, icfgNode.get());
+
+    ASSERT_NE(icfgNode.get(), nullptr);
+    ASSERT_EQ(node.get(), nullptr);
+    unique_ptr<ICFGNode> ep;
+    ep = std::move(icfgNode);
+    ASSERT_EQ(icfgNode.get(), nullptr);
+    ASSERT_NE(ep.get(), nullptr);
+
+    unique_ptr<ICFGNode> n2 = make_unique<ICFGNode>();
+    ASSERT_NE(n2.get(), nullptr);
+    auto addr1 = n2.get();
+    // after the assignment, we check that the address n2
+    // is holding is different
+    n2 = std::move(ep);
+    ASSERT_NE(n2.get(), addr1);
+
+
+    {
+        unique_ptr<ICFGNode> globalNode = make_unique<GlobalBlockNode>();
+        unique_ptr<ICFGNode> funEntryNode  = make_unique<FunEntryBlockNode>();
+        unique_ptr<ICFGNode> funExitNode = make_unique<FunExitBlockNode>();
+
+        unique_ptr<GlobalBlockNode> g_c = llvm::dyn_cast<GlobalBlockNode>(globalNode);
+        ASSERT_NE(g_c.get(), nullptr);
+
+        unique_ptr<FunEntryBlockNode> fentry = llvm::dyn_cast<FunEntryBlockNode>(funEntryNode);
+        ASSERT_NE(fentry.get(), nullptr);
+
+        unique_ptr<FunExitBlockNode> fexit = llvm::dyn_cast<FunExitBlockNode>(funExitNode);
+        ASSERT_NE(fexit.get(), nullptr);
+
+        unique_ptr<FunEntryBlockNode> fc = llvm::dyn_cast<FunEntryBlockNode>(globalNode);
+        ASSERT_EQ(fc.get(), nullptr);
+    }
+}
+
+TEST(CastTestSuite, shared_ptr_basic_0) {
+    shared_ptr<ICFGNode> globalNode = make_shared<GlobalBlockNode>();
+    shared_ptr<ICFGNode> funEntryNode  = make_shared<FunEntryBlockNode>();
+    shared_ptr<ICFGNode> funExitNode = make_shared<FunExitBlockNode>();
+
+    ASSERT_TRUE(llvm::isa<GlobalBlockNode>(globalNode));
+    ASSERT_TRUE(llvm::isa<ICFGNode>(globalNode));
+    ASSERT_TRUE(llvm::isa<FunEntryBlockNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<InterBlockNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<ICFGNode>(funEntryNode));
+    ASSERT_FALSE(llvm::isa<GlobalBlockNode>(funExitNode));
+    ASSERT_TRUE(llvm::isa<FunExitBlockNode>(funExitNode));
+    ASSERT_TRUE(llvm::isa<InterBlockNode>(funEntryNode));
+    ASSERT_TRUE(llvm::isa<ICFGNode>(funEntryNode));
+
+    shared_ptr<GlobalBlockNode> g_c = llvm::dyn_cast<GlobalBlockNode>(globalNode);
+    ASSERT_NE(g_c.get(), nullptr);
+
+    shared_ptr<FunEntryBlockNode> fentry = llvm::dyn_cast<FunEntryBlockNode>(funEntryNode);
+    ASSERT_NE(fentry.get(), nullptr);
+
+    shared_ptr<FunExitBlockNode> fexit = llvm::dyn_cast<FunExitBlockNode>(funExitNode);
+    ASSERT_NE(fexit.get(), nullptr);
+
+    ASSERT_NE(g_c.get(), nullptr);
+    shared_ptr<FunEntryBlockNode> fc = llvm::dyn_cast<FunEntryBlockNode>(g_c);
+    ASSERT_EQ(fc.get(), nullptr);
+}
+
+TEST(CastTestSuite, ThreadMHPIndSVFGEdge_for_delete_redundant) {
+    GenericVFGEdgeTy *gVFGEdge = new ThreadMHPIndSVFGEdge();
+
+    ASSERT_NE(gVFGEdge, nullptr);
+    // VFGEdge *vfgEdge = llvm::dyn_cast<VFGEdge>(gVFGEdge);
+    IndirectSVFGEdge *indSVFGEdge = llvm::dyn_cast<IndirectSVFGEdge>(gVFGEdge);
+    ThreadMHPIndSVFGEdge *tmhpEdge = llvm::dyn_cast<ThreadMHPIndSVFGEdge>(gVFGEdge);
+    // VFGEdge *tmhpEdge2 = llvm::dyn_cast<ThreadMHPIndSVFGEdge>(VFGEdge);
+    cout << indSVFGEdge << endl;
+    cout << tmhpEdge << endl;
+    ASSERT_NE(indSVFGEdge, nullptr);
+    ThreadMHPIndSVFGEdge *tmhpEdge2 = llvm::dyn_cast<ThreadMHPIndSVFGEdge>(indSVFGEdge);
+
+    delete gVFGEdge;
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
